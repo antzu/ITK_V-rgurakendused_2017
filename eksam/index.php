4c571b5accaf6845d404000ac8baaa411d4404e2
antzu <andres.aava@gmail.com>
2017-06-04 18:36:51 +0300

    stable for editing posts, fixed adding posts

--- eksam/index.php
+++ eksam/index.php
@@ -3,35 +3,38 @@
 	connect_db();
 	session_start();
 
-	
-
-	if (isset($_GET['postid'])) {
-		$postid = $_GET['postid'];
-		$sql = "SELECT `post` FROM `posts` WHERE id='$postid' ";
-    	$result = mysqli_query($connection, $sql) or die("Sellist postitust pole!!!");
-		$r = mysqli_fetch_assoc($result);
-
-		$changablepost = $r['post'];
-	}
-
-	if (isset($_POST['post'])) {
-		if (isset($_GET['postid'])) {
-
+	if (!empty($_GET['postid'])) {
+		
+		
 			$postid = $_GET['postid'];
-			$post = mysqli_real_escape_string ($connection, $_POST["post"]);
-			$sql = "UPDATE `posts` SET id='$postid', post='$post' WHERE id='$postid' ";
-		            
-		    $result = mysqli_query($connection, $sql);
+			$sql = "SELECT `post` FROM `posts` WHERE id='$postid' ";
+	    	$result = mysqli_query($connection, $sql) or die("Sellist postitust pole!!!");
+			$r = mysqli_fetch_assoc($result);
 
-		} else {
+			$changablepost = $r['post'];
+		
+
+		if (isset($_POST['muuda'])) {
+			
+
+				$postid = $_GET['postid'];
+				$post = mysqli_real_escape_string ($connection, $_POST["post"]);
+				$sql = "UPDATE `posts` SET id='$postid', post='$post' WHERE id='$postid' ";
+			            
+			    $result = mysqli_query($connection, $sql);
+
+			
+			   	
+		}
+
+} elseif(isset($_POST['postita'])) {
 
 		$post = mysqli_real_escape_string ($connection, $_POST["post"]);
         $sql = "INSERT INTO `posts` (`post`) VALUES ('$post')";
         $result = mysqli_query($connection, $sql);
         $success = mysqli_insert_id($connection);
 
-    	}
-	}
+}
 
 	$sql = "SELECT `id`, `post` FROM `posts` ";
     $result = mysqli_query($connection, $sql) or die("Sellist postitust pole!!!");
@@ -62,13 +65,24 @@
 <body>
 	<div>
 		<h1>Märkmed</h1>
+		
 		<form method="post" action="">
-			<h3>Postita siia:</h3>
+			<h3>Muuda postitust:</h3>
 			<br>
 			<textarea name = "post" placeholder = "your post here" cols="80" rows="10" required><?php echo $changablepost; ?></textarea>
 			<br>
-			<button type="submit" name="submit">Postita</button>
+			<button type="submit" name="muuda">Muuda</button>
 		</form>
+
+		<form method="post" action="?">
+			<h3>Postita siia:</h3>
+			<br>
+			<textarea name = "post" placeholder = "your post here" cols="80" rows="10" required></textarea>
+			<br>
+			<button type="submit" name="postita">Postita uus</button>
+		</form>
+
+
 		<div>
 			<?php
 				foreach ($posts as $n => $post) {
